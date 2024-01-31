34d8f9cb31351fe6779f6fb0cd385084f1fcd687
ELS <57393100+kodosa7@users.noreply.github.com>
2024-01-31 12:30:04 +0100

    CSS adjust of eggshell element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import Logo from "/src/assets/jglogo.svg";
 
 export default function Header() {
     return (
-        <div className="header-container py-4">
-            <div className="header">
-                <a href="https://junior.guru/" className="logo">
-                    <img src={Logo} alt="JuniorGuru Logo" className="w-28" />
-                </a>
-                <div className="label text-lg">
-                    Timer
+        <>
+            <div className="header-container py-4">
+                <div className="header">
+                    <a href="https://junior.guru/" className="logo">
+                        <img src={Logo} alt="JuniorGuru Logo" className="w-28" />
+                    </a>
+                    <div className="label text-lg">
+                        Timer
+                    </div>
+                    <div className="empty-space"></div>
                 </div>
-                <div className="empty-space"></div>
             </div>
             <div className="eggshell"></div>
-        </div>
+        </>
     )
 }
